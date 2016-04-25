27a230e3db0d6dd4e080edfcccfaf46cc7b8b6b0
Andreas Lövqvist <andreas.lovqvist@gmail.com>
2016-04-25 11:26:04 +0200

    Filter™ v0.1.1 -added comments and exception for falsy filter

--- www/controllers/buy.js
+++ www/controllers/buy.js
@@ -5,11 +5,22 @@ app.controller("buy", ["$scope", "property", function($scope, property) {
 	$(document).ready(function() {
 
 		function loadProperties(data) {
-			if (data) {
-				$scope.values = data;
+			if (data) { // if data from filter exsist
+				if (data[0]) { // if any property with filter exsist
+					$scope.values = data;
+				}
+				else { // if any property with filter do not exsists
+					$scope.values = data;
+
+					// use {{values.noFind}} for falsy filter
+					$scope.values.push({ noFind : "Could not find any property." });
+
+					console.log($scope.values.noFind);
+				}
 			} 
 			else {
 
+				// fetch data from db
 				$scope.values = property.get(function(data){
 
 					console.log(data);
@@ -19,15 +30,9 @@ app.controller("buy", ["$scope", "property", function($scope, property) {
 		}
 
 
-
-
-
-
-
-
 		$('.btnFilter').on('click', function() {
 
-			property.get( 
+			property.get( // fetcg data from db with filter
 				{ $and: [ { price: { $gt : 5 }}, 
 				{ price: { $lt : 1000000 }} /* , add more filter here */ ]}, 
 				function(data){
