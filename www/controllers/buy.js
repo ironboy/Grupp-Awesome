88e5e5037cb15b7678bba345ad64125d9af539a3
Andreas Lövqvist <andreas.lovqvist@gmail.com>
2016-04-25 10:12:35 +0200

    Filter™ v.0.1

--- www/controllers/buy.js
+++ www/controllers/buy.js
@@ -2,6 +2,21 @@ app.controller("buy", ["$scope", "property", function($scope, property) {
 	
 
 	
+	$(document).ready(function() {
+
+		function loadProperties(data) {
+			if (data) {
+				$scope.values = data;
+			} 
+			else {
+
+				$scope.values = property.get(function(data){
+
+					console.log(data);
+					
+				});
+			}
+		}
 
 
 
@@ -10,19 +25,19 @@ app.controller("buy", ["$scope", "property", function($scope, property) {
 
 
 
+		$('.btnFilter').on('click', function() {
 
+			property.get( 
+				{ $and: [ { price: { $gt : 5 }}, 
+				{ price: { $lt : 1000000 }} /* , add more filter here */ ]}, 
+				function(data){
 
+					console.log(data);
 
-	/*
-	
-		$('btnFilter').on('click', function(){
-			property.get(function(data){
-
-				console.log(data);
-				
+					loadProperties(data);
 			});
-		})
-		
+		});
 
-	*/
+		loadProperties();
+	});
 }]);
