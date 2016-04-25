d7cf30fc33fea421ee0e3be3cfb45138da8caae5
Andreas Lövqvist <andreas.lovqvist@gmail.com>
2016-04-25 13:09:42 +0200

    Filter™ v0.1.1.1 -minor fix

--- www/controllers/buy.js
+++ www/controllers/buy.js
@@ -5,35 +5,38 @@ app.controller("buy", ["$scope", "property", function($scope, property) {
 	$(document).ready(function() {
 
 		function loadProperties(data) {
-			if (data) { // if data from filter exsist
-				if (data[0]) { // if any property with filter exsist
-					$scope.values = data;
-				}
-				else { // if any property with filter do not exsists
-					$scope.values = data;
+			if (data) { 
 
-					// use {{values.noFind}} for falsy filter
+				// if data from filter exsist
+				$scope.values = data;
+
+				if (!data[0]) { 
+
+					// if any property with filter do not exsists
+					
+					// use {{x.noFind}} for falsy filter
 					$scope.values.push({ noFind : "Could not find any property." });
-
-					console.log($scope.values.noFind);
+					
 				}
 			} 
 			else {
 
 				// fetch data from db
-				$scope.values = property.get(function(data){
-
-					console.log(data);
-					
-				});
+				$scope.values = property.get(function(data){ console.log(data); });
 			}
 		}
 
 
 		$('.btnFilter').on('click', function() {
 
-			property.get( // fetcg data from db with filter
-				{ $and: [ { price: { $gt : 5 }}, 
+			if (!$(".justAnInput")[0].value) {
+				$(".justAnInput")[0].value = 0;
+			}
+
+			property.get( 
+
+				// fetch data from db with filter
+				{ $and: [ { price: { $gt : $(".justAnInput")[0].value }}, 
 				{ price: { $lt : 1000000 }} /* , add more filter here */ ]}, 
 				function(data){
 
